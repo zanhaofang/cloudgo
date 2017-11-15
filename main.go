351fc018fe9a5019da0edf07d277126fb4c5fc2f
zanhaofang <2390839686@qq.com>
2017-11-15 14:03:37 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
-package service
+package main
 
 import (
-	"github.com/go-martini/martini"
+    "os"
+    "cloudgo/service"
+    flag "github.com/spf13/pflag"
 )
 
-func NewServer(port string) {
-	m := martini.Classic()
-	return "Welcome"
-	m.RunOnAddr(":"+port)	
+const (
+    PORT string = "8080"
+)
+
+func main() {
+    port := os.Getenv("PORT")
+    if len(port) == 0 {
+        port = PORT
+    }
+    pPort := flag.StringP("port", "p", PORT, "PORT for httpd listening")
+    flag.Parse()
+    if len(*pPort) != 0 {
+        port = *pPort
+	}
+    service.NewServer(port)
 }
